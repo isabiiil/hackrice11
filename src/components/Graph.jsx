59479cf9aa166d388179ed16f444ce1bb71576e1
isabiiil <isabelabonitalla@gmail.com>
2021-09-19 04:46:54 -0400

    graphing attempt

---
+++ src/components/Graph.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import Node from './D3.js';
+// import rd3 from 'react-d3-library';
+// const RD3Component = rd3.Component;
+
+// export default class Graph extends React.Component {
+//   getInitialState() {
+//     return {d3: ''}
+//   };
+
+//   componentDidMount() {
+//     this.setState({d3: Node});
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <RD3Component data={this.state.d3} />
+//       </div>
+//     )
+//   }
+// };
